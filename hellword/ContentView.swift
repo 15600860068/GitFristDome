76649b5e27b9ea09fc6a5a9e0b04fccebeb11212
iCode <961217564@qq.com>
2020-06-02 17:43:06 +0800

    第一个提交到Git的Dome

---
+++ hellword/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  hellword
+//
+//  Created by 仁美大学堂 on 2019/11/25.
+//  Copyright © 2019 仁美大学堂. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ContetView : UIView {
+    static var previews: some View {
+        if View {
+            
+        }
+    }
+}
